edceda6b124772a0a814d8dd4f255a1d8439e322
needlzzz <48053720+needlzzz@users.noreply.github.com>
2019-12-08 20:27:36 +0100

    Update move_ARW_files.py

--- move_ARW_files.py
+++ move_ARW_files.py
@@ -3,25 +3,26 @@ import os
 import shutil
 
 # creates the folders for the new pictures to be moved to
-# note that the drive cannot be part of the path
+# note that when you give the windows path name with backslashes, you need to use "r" for raw string.
+# That way it won't red the "\" as escape characters.
 
-newest_pictures_directory = '/test_folder/newest_pictures'
-RAW_directory = '/test_folder/newest_pictures/RAW'
-JPG_directory = '/test_folder/newest_pictures/JPG'
+newest_pictures_directory = r""
+RAW_directory = r"\RAW"
+JPG_directory = r"\JPG"
 os.mkdir(newest_pictures_directory)
 os.mkdir(RAW_directory)
 os.mkdir(JPG_directory)
 
-arw_files = glob.iglob(os.path.join('/Users/marc/Downloads', "*.pdf"))
-jpg_files = glob.iglob(os.path.join('/Users/marc/Downloads', "*.jpg"))
+raf_files = glob.iglob(os.path.join(r"T:\DCIM\101_FUJI", "*.RAF"))
+jpg_files = glob.iglob(os.path.join(r"T:\DCIM\101_FUJI", "*.JPG"))
 
-for file in arw_files:
+for file in raf_files:
     if os.path.isfile(file):
-        shutil.move(file, '/test_folder/newest_pictures/RAW')
+        shutil.move(file, r"\RAW")
 
 for jpg in jpg_files:
     if os.path.isfile(jpg):
-        shutil.move(jpg, '/test_folder/newest_pictures/JPG')
+        shutil.move(jpg, r"\JPG")
 
 print("All files successfully moved from Memory Card")
 input("Press Enter to continue")
